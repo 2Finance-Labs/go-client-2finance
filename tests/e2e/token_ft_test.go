ae13f49cf2d102fed5ec30e02d575fafad34cdb5
Gabriel Hausmann <gabrielhausmann11@gmail.com.com>
2026-05-15 14:23:24 -0300

    removing Equals from FrozenAccount

--- tests/e2e/token_ft_test.go
+++ tests/e2e/token_ft_test.go
@@ -1011,7 +1011,6 @@ func TestTokenFlowFungible(t *testing.T) {
 	}
 
 	assert.Equal(t, tok.Address, freezeEvent.TokenAddress, "freeze wallet event token address mismatch")
-	assert.Equal(t, owner.PublicKey, freezeEvent.FrozenAccount, "freeze wallet event wallet address mismatch")
 
 	getTokenOut16, err := c.GetToken(tok.Address, "", "")
 	if err != nil {
@@ -1047,7 +1046,6 @@ func TestTokenFlowFungible(t *testing.T) {
 		t.Fatalf("UnmarshalEvent (UnfreezeWallet.Logs[0]): %v", err)
 	}
 	assert.Equal(t, tok.Address, unfreezeEvent.TokenAddress, "unfreeze wallet event token address mismatch")
-	assert.Equal(t, owner.PublicKey, unfreezeEvent.FrozenAccount, "unfreeze wallet event wallet address mismatch")
 
 	getTokenOut17, err := c.GetToken(tok.Address, "", "")
 	if err != nil {
